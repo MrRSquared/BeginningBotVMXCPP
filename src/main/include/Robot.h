a012989b6093f1ce6620be7484a59b10a61ecacb
MrRSquared <jrothritchie@hdsd.org>
2021-08-17 09:12:28 -0400

    Initial commit

---
+++ src/main/include/Robot.h
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+//Include the TitanQuad Library
+#include <studica/TitanQuad.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+  //Constuct a new instance
+  studica::TitanQuad motor1{42, 0};
+  studica::TitanQuad motor2{42, 1};
+  studica::TitanQuad motor3{42, 0};
+  studica::TitanQuad motor4{42, 0};
+};
